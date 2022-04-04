7f7b544d9405bf31f7536bab556bd351103fb9e1
Gleb Limanskiy <33000609+UnicalN@users.noreply.github.com>
2022-04-04 21:58:44 +0300

    Cb updatepins added to getdata in main

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 import {validateOffer, setUserFormSubmit} from './modules/interaction-with-form.js';
 import {getData} from './modules/server-interfaces.js';
-import { resetMap, map} from './modules/map.js';
+import { resetMap, map, updatePins} from './modules/map.js';
 import {enablePage, disablePage} from './modules/enable-disable-page.js';
 disablePage();
 
@@ -8,7 +8,7 @@ disablePage();
 map.whenReady(() => {
   enablePage();
   validateOffer();
-  getData();
+  getData(updatePins);
   resetMap();
   setUserFormSubmit();
 });
